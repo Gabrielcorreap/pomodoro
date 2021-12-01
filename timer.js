34fd529c35119c650fcdf5391cf3b79eef1d475d
Gabriel Corrêa paes <87083919+Gabrielcorreap@users.noreply.github.com>
2021-12-01 13:25:37 -0300

    final project

---
+++ timer.js
@@ -0,0 +1,67 @@
+const el = document.querySelector(".clock");
+const bell = document.querySelector("audio");
+
+const mindiv = document.querySelector(".mins");
+const secdiv = document.querySelector(".secs");
+
+const startBtn = document.querySelector(".start");
+
+const startbtn = document.querySelector(".btnplay")
+
+
+const state = document.querySelector(".state")
+
+
+localStorage.setItem("btn", "focus");
+
+let initial, totalsecs, perc, paused, mins, seconds;
+
+startBtn.addEventListener("click", () => {
+  let btn = localStorage.getItem("btn");
+
+  if (btn === "focus") {
+    mins = +localStorage.getItem("focusTime") || 1;
+  } else {
+    mins = +localStorage.getItem("breakTime") || 1;
+  }
+
+  seconds = mins * 60;
+  totalsecs = mins * 60;
+  setTimeout(decremenT(), 60);
+  startbtn.style.transform = "scale(0)"
+  paused = false;
+});
+
+function decremenT() {
+  mindiv.textContent = Math.floor(seconds / 60);
+  secdiv.textContent = seconds % 60 > 9 ? seconds % 60 : `0${seconds % 60}`;
+  if (circle.classList.contains("danger")) {
+    circle.classList.remove("danger");
+  }
+
+  if (seconds > 0) {
+    perc = Math.ceil(((totalsecs - seconds) / totalsecs) * 100);
+    setProgress(perc);
+    seconds--;
+    initial = window.setTimeout("decremenT()", 1000);
+    if (seconds < 10) {
+      circle.classList.add("danger");
+    }
+  } else {
+    mins = 0;
+    seconds = 0;
+    bell.play();
+    let btn = localStorage.getItem("btn");
+
+    if (btn === "focus") {
+      state.textContent = "intervalo";
+      startBtn.classList.add("break");
+      localStorage.setItem("btn", "break");
+    } else {
+      startBtn.classList.remove("break");
+      state.textContent = "foco"
+      localStorage.setItem("btn", "focus");
+    }
+    startBtn.style.transform = "scale(1)";
+  }
+}
